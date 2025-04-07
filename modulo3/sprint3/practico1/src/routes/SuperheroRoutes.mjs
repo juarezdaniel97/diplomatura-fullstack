a8fcf0750fa5367041a200fb87b0be1359699095
Daniel Juarez <juarezz.daniel97@gmail.com>
2025-04-07 17:04:21 -0300

    modulo3-sprint3-practico1-terminado

---
+++ modulo3/sprint3/practico1/src/routes/SuperheroRoutes.mjs
@@ -0,0 +1,51 @@
+import express from 'express';
+import { 
+    obtenerListadoSuperHeroController,
+    obtenerSuperHeroPorIdController, 
+    buscarSuperHeroPorAtributoController,
+    obtenerSuperHeroMayoresA30Controller,
+    agregarSuperHeroController,
+    eliminarSuperHeroPorIdController,
+    eliminarSuperHeroPorNombreController,
+    actualizarSuperHeroController
+} from '../controllers/superheroesController.mjs';
+
+import {validationErrorHandler} from '../middlewares/errorHandler.mjs';
+import {
+    validateNombreSuperHero,
+    validateNombreReal,
+    validateEdad,
+    validatePoderes
+} from '../validators/heroValidators.mjs'
+
+
+
+const router = express.Router();
+
+router.get('/heroes', obtenerListadoSuperHeroController);
+router.get('/heroes/:id', obtenerSuperHeroPorIdController);
+router.get('/heroes/buscar/:atributo/:valor', buscarSuperHeroPorAtributoController);
+router.get('/heroes/mayores/30', obtenerSuperHeroMayoresA30Controller);
+router.post('/heroes/',
+    [
+        validateNombreSuperHero,
+        validateNombreReal,
+        validateEdad,
+        validatePoderes
+    ],
+    validationErrorHandler,
+    agregarSuperHeroController );
+router.delete('/heroes/delete/:id', eliminarSuperHeroPorIdController);
+router.delete('/heroes/delete-nombre/:nombre', eliminarSuperHeroPorNombreController);
+router.put('/heroes/update/:id', 
+    [
+        validateNombreSuperHero,
+        validateNombreReal,
+        validateEdad,
+        validatePoderes
+    ],
+    validationErrorHandler,
+    actualizarSuperHeroController);
+
+
+export default router;
